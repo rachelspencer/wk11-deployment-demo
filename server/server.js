05e3fd44efcad47282179741c24f1d532e30c01c
Rachel Spencer <rachelspencer688@gmail.com>
2022-11-01 22:37:56 -0400

    show h1 text in green

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ app.get('/', function(req, res) {
 });
 
 app.get("/styles", function(req,res) {
-    res.sendFile(path.join(__durname, "../public/index.css"))
-})
+    res.sendFile(path.join(__dirname, "../public/index.css"))
+});
 
 const port = process.env.PORT || 4005;
 
