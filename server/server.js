ab01fa56848b1621a126717f821a4717e76e7fb6
Rachel Spencer <rachelspencer688@gmail.com>
2022-11-01 21:52:14 -0400

    Commit with Procfile

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`Port running on ${port}`)
+});
